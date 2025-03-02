0ed5547e3b7b5bc61397a5bf41cfdc59dac24ec8
Egor <tolmacevegor46@gmail.com>
2025-03-02 18:44:58 +0300

    Initial commit

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,7 @@
+const cookie = document.getElementById('cookie');
+const count = document.getElementById('clicker__counter');
+
+cookie.addEventListener('click', () => {
+    count.textContent++;
+    cookie.width = cookie.width == 200 ? 250 : 200;
+});
